2f4eb8161ae5a82527b0f61415f1cd7c216edf1e
Bhavya Ravilla <bravilla@002250-mac.local>
2020-01-16 17:29:35 -0800

    router concepts

--- AngularComponentExample/src/app/app.module.ts
+++ AngularComponentExample/src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
+import { ListEmployeesComponent } from './list-employees/list-employees.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
+
+const appRoutes: Routes = [
+  { path: 'list', component: ListEmployeesComponent},
+  { path: 'create', component: CreateEmployeeComponent},
+  { path: '', redirectTo: '/list', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     BooksComponent,
-    MainNavComponent
+    MainNavComponent,
+    ListEmployeesComponent,
+    CreateEmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
